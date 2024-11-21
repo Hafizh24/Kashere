a0f4fb137920c46c97db063f622dec6dccc5ac0a
Hafizh24 <hafizhcbs@gmail.com>
2024-11-21 23:12:06 +0700

    remove home page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,3 @@
 
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
-
-
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
-Route::get('/home', Home::class)->name('home');
